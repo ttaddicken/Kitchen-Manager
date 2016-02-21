5e713767b5efcc34e5659bd88557026f08ca00f3
John Kent <jf.kent@yahoo.com>
2016-02-21 12:01:20 -0700

    Additional updates to create and manage dish functionality

--- public/app/views/dishes/dishCreation-controller.js
+++ public/app/views/dishes/dishCreation-controller.js
@@ -1,3 +1,31 @@
 app.controller('DishCreationController', function($scope) {
 
+// links recipies/dishes from firebase
+$scope.dishes = DataService.getDishes();
+
+
+// ADD createRecipe() function **need to determine how firebase defines our "userName through registration() on login page.
+	  $scope.addRecipe = function(newRecipe){
+	 	newRecipe.memberId = $rootScope.member.$id
+        $scope.newRecipe.userName = $rootScope.userName; 
+        $scope.newRecipe.date = Date.now();
+	 	$scope.recipes.$add(newRecipe).then(function(ref){
+	  	  //Add the newly added recipe to the member object	
+	  	  $rootScope.member.recipes = $rootScope.member.recipes || {};
+	    //An dictonary structure is needed to add the recipeId to the member.recipes dictionary.  (ie our favorites)
+	     //To avoid duplicating data in our favorites database we only store the questionId instead of the entire question again 
+	     $rootScope.member.recipes[ref.key()] = ref.key();
+	     $rootScope.member.$save();
+
+         })
+      
+      }
+
+// clearRecipe()
+    $scope.clearRecipe = function(clearRecipe){
+
+// WE NEED THESE FUNCTIONS:
+// addToFavorites()
+// addIngredient()
+// importToFavorites()
 })
