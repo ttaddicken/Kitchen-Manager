d3fe06ec9f48ed02dfcf1e81bfb9915e1a2cdd8a
John Kent <jf.kent@yahoo.com>
2016-02-25 15:43:47 -0700

    sets logout function

--- public/app/views/registration/registration-controller.js
+++ public/app/views/registration/registration-controller.js
@@ -1,105 +1,112 @@
 /* global Firebase */
-app.controller('RegistrationController', function ($scope, $state, AuthService) {
+app.controller('RegistrationController', function ($scope, $rootScope, $state, AuthService) {
 
-	$scope.login = function (user) {
+    $scope.login = function (user) {
        	clearErr();
-		AuthService.login($scope.user, handleDBResponse);
-	};
+        AuthService.login($scope.user, handleDBResponse);
+    };
 
-	$scope.register = function (user) {
-		clearErr();
-		AuthService.register($scope.user, handleDBResponse);
-	};
+    $scope.register = function (user) {
+        clearErr();
+        AuthService.register($scope.user, handleDBResponse);
+    };
 
-	$scope.facebookLogin = function () {
-		clearErr();
-		AuthService.facebookLogin(handleDBResponse);
-	};
+    $scope.facebookLogin = function () {
+        clearErr();
+        AuthService.facebookLogin(handleDBResponse);
+    };
 
-	function clearErr(){
-		$scope.authErr = '';
-	}
+    function clearErr() {
+        $scope.authErr = '';
+    }
 
-	function handleDBResponse(err) {
-		if (err) {
-			$scope.authErr = err.message;
-			$scope.$apply();
-		} else {
-			$state.go('home');
-		}
-	}
+    function handleDBResponse(err) {
+        if (err) {
+            $scope.authErr = err.message;
+            $scope.$apply();
+        } else {
+            $state.go('home');
+        }
+    }   
+// Logout Function    
+//     $scope.logout = function () {
+//         $rootScope.member = null;
+//         $state.go('login')
+//         }
+// },
 });
 
 app.controller('AuthController', function ($rootScope, $state, AuthService) {
-	//Redirect if Unable to Authenticate
-	if (!$rootScope.member) {
-		AuthService.authMember(function (err) {
-			if (err) {
-				//TODO: TOAST ERROR
-				$state.go('login');
-			}
-		});
-	}
+    //Redirect if Unable to Authenticate
+    if (!$rootScope.member) {
+        AuthService.authMember(function (err) {
+            if (err) {
+                //TODO: TOAST ERROR
+                $state.go('login');
+            }
+        });
+    }
 });
 
 
 app.service('AuthService', function ($rootScope, $firebaseObject, CONSTANTS) {
 
-	var db = new Firebase(CONSTANTS.fbRef)
+    var db = new Firebase(CONSTANTS.fbRef)
 
-	function authMember(cb) {
-		var authData = db.getAuth();
-		if (authData) {
-			var id = authData.uid;
-			if (id.indexOf(':') > -1) {
-				var cutOffProviderIndex = authData.uid.indexOf(':') + 1;
-				id = id.slice(cutOffProviderIndex);
-			}
-			setMember(id, cb);
-		} else {
-			cb ? cb({ error: { message: 'Unable to Authenticate' } }) : '';
-			return true;
-		}
-	}
+    function authMember(cb) {
+        var authData = db.getAuth();
+        if (authData) {
+            var id = authData.uid;
+            if (id.indexOf(':') > -1) {
+                var cutOffProviderIndex = authData.uid.indexOf(':') + 1;
+                id = id.slice(cutOffProviderIndex);
+            }
+            setMember(id, cb);
+        } else {
+            cb ? cb({ error: { message: 'Unable to Authenticate' } }) : '';
+            return true;
+        }
+    }
 
-	function setMember(id, cb) {
-		$rootScope.member = $firebaseObject(new Firebase(CONSTANTS.fbRef + 'users/' + id));
-		cb? cb() : '';
-	}
-	
-	function createUser(authData, user){
-		var userToAdd = {
-				email: user.email,
-				reputation: 0,
-				created: Date.now()
-			}
-			db.child('users').child(authData.uid).set(userToAdd);
-	}
+    function setMember(id, cb) {
+        $rootScope.member = $firebaseObject(new Firebase(CONSTANTS.fbRef + 'users/' + id));
+        cb ? cb() : '';
+    }
 
-	this.authMember = authMember;
+    function createUser(authData, user) {
+        var userToAdd = {
+            email: user.email,
+            reputation: 0,
+            created: Date.now()
+        }
+        db.child('users').child(authData.uid).set(userToAdd);
+    }
 
-	this.register = function (user, cb) {
-		db.createUser(user, function (err, authData) {
-			if (err) {
-				return cb(err)
-			}
-			createUser(authData, user);
-			authMember(cb);
-		});
-	}
+    this.authMember = authMember;
 
-	this.login = function (user, cb) {
-		db.authWithPassword(user, function (err, authData) {
-			if (err) {
-				return cb(err)
-			}
-			authMember(cb);
-		})
-	}
+    this.register = function (user, cb) {
+        db.createUser(user, function (err, authData) {
+            if (err) {
+                return cb(err)
+            }
+            createUser(authData, user);
+            authMember(cb);
+        });
+    }
 
-	this.logout = function(){
-		db.unauth();
-		$rootScope.member = null;
-	}
-	
+    this.login = function (user, cb) {
+        db.authWithPassword(user, function (err, authData) {
+            if (err) {
+                return cb(err)
+            }
+            authMember(cb);
+        })
+    }
+
+    this.logout = function () {
+        db.unauth();
+        $rootScope.member = null;
+        $state.go('login')
+    }
+
 });
