d58ad1e595d2069999c814a47d1260fa2b9552be
John Kent <jf.kent@yahoo.com>
2016-03-05 00:10:51 -0700

    stuff with ratings controller

--- public/app/views/dishes/dishCreation-controller.js
+++ public/app/views/dishes/dishCreation-controller.js
@@ -46,22 +46,22 @@ app.controller('DishCreationController', function ($scope, $rootScope, CONSTANTS
 
 //CREATE A STAR RATING CAPABILITY TO CREATE A DISH **Also needed for Manage Dishes
 // ??? DO WE REALLY NEED A CONTROLLER FOR THIS???
-app.controller('RatingDemoCtrl', function ($scope, $rootScope, CONSTANTS, $firebaseArray) {
-    $scope.rate = 5;
-    $scope.max = 5;
-    $scope.isReadonly = false;
+// app.controller('RatingDemoCtrl', function ($scope, $rootScope, CONSTANTS, $firebaseArray) {
+//     $scope.rate = 5;
+//     $scope.max = 5;
+//     $scope.isReadonly = false;
 
-    $scope.hoveringOver = function (value) {
-        $scope.overStar = value;
-        $scope.percent = 100 * (value / $scope.max);
-    };
+//     $scope.hoveringOver = function (value) {
+//         $scope.overStar = value;
+//         $scope.percent = 100 * (value / $scope.max);
+//     };
 
-    $scope.ratingStates = [
-        { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
-        { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
-        { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
-        { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
-        { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' }
-    ];
-})
+//     $scope.ratingStates = [
+//         { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
+//         { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
+//         { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
+//         { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' },
+//         { stateOn: 'fa fa-spoon fa-2x', stateOff: 'fa fa-lemon-o fa-2x' }
+//     ];
+// })
   });
