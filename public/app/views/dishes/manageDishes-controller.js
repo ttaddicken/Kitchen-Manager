135e53e0617dd397ab99bd1b97b67540a23f951e
Sara Randolph <sara.randolph222@gmail.com>
2016-03-04 22:28:17 -0700

    small changes

--- public/app/views/dishes/manageDishes-controller.js
+++ public/app/views/dishes/manageDishes-controller.js
@@ -15,6 +15,14 @@ app.controller('ManageDishesController', function ($rootScope, $scope, DishServi
         });
     }
     
+    
+    $('#summernote').summernote({
+        height: 300,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: true                  // set focus to editable area after initializing summernote
+    });
+    
 })
 //  Begginning of borrowed from firebase
 // {
