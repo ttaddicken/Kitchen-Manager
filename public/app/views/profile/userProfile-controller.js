721b43b417f24029b4ab6bb2a7376fb5ecb64051
ttaddicken <tjtaddicken@hotmail.com>
2016-03-15 15:30:16 -0600

    stars functiong and one graph fixed

--- public/app/views/profile/userProfile-controller.js
+++ public/app/views/profile/userProfile-controller.js
@@ -1,81 +1,69 @@
 'use strict';
-app.controller('ProfileController', function($rootScope, $scope, DishService, CONSTANTS, $firebaseArray){
+app.controller('ProfileController', function($rootScope, $scope, DishService, CONSTANTS, $firebaseArray) {
 
+    DishService.getMyDishes().$loaded(function(dishes) {
+        $scope.myDishes = dishes;
+        buildZeros();
+        starCounter();
+        buildGraphs();
+    });
 
-    
-$rootScope.myDishes = DishService.getMyDishes();
+    // $scope.ratingStates = DishService.ratingStates;
 
-$scope.ratingStates = DishService.ratingStates
+    $scope.zeroStars = [];
 
-var zeroStars = [];
-    
-function buildZeros(){
-    for(var i = 0; i < $rootScope.myDishes.length; i++){
-        if($rootScope.myDishes[i].stars === 0){
-            zeroStars.push(myDishes[i])
+    function buildZeros() {
+        for (var i = 0; i < $scope.myDishes.length; i++) {
+            if (!$scope.myDishes[i].stars) {
+                $scope.zeroStars.push($scope.myDishes[i])
+            }
         }
     }
-return zeroStars;
-}
-buildZeros();    
-    
-    
-    
-//     var oneStar = [];
-//     var twoStars = [];
-//     var threeStars = [];
-//     var fourStars = [];
-//     var fiveStars = [];
-    
-//     var starCounter = function() {
-        
-//     for(var i = 0; i <= $rootScope.myDishes.length; i++){
-        
-//         var currDish = $rootScope.myDishes[i];
-        
-//         if(currDish[stars] === 1){
-//             oneStar.push(currDish);
-//         } 
-//         if(currDish[stars]  === 2){
-//             twoStars.push(currDish);
-//         }
-//         if(currDish[stars]  === 3){
-//             threeStars.push(currDish);
-//         } 
-//         if(currDish[stars]  === 4){
-//             fourStars.push(currDish);
-//         }
-//         if(currDish[stars]  === 5){
-//             fiveStars.push(currDish);
-//         }           
-//         else{ zeroStars.push(currDish);
-//         }
-//     }    
-// };
 
-// starCounter();
-    
-    $scope.zeroStars = 10;
-    $scope.oneStar = 5;
-    $scope.twoStars = 10;
-    $scope.threeStars = 5;
-    $scope.fourStars = 10;
-    $scope.fiveStars = 5;
-    
-    
-//  CHART RECIPES BY STAR RATING: 
- $scope.starOptions = {
+    $scope.starRatings = {};
+
+    var starCounter = function() {
+        //AngularFire brute force $firebaseArray != [] its a tricky trick 
+        //$scope.myDishes === {} != []; 
+        for (var i = 0; i <= $scope.myDishes.length - 1; i++) {
+            try {
+                var currDish = $scope.myDishes[i];
+                var starCount = currDish.stars || 'unrated';
+                $scope.starRatings[starCount] = $scope.starRatings[starCount] || {};
+                $scope.starRatings[starCount].dishes = $scope.starRatings[starCount].dishes || [];
+                $scope.starRatings[starCount].dishes.push(currDish);
+            } catch (error) {
+                debugger;
+            }
+        }
+        console.log($scope.myDishes);
+    };
+    // starCounter();
+
+    // $scope.zeroStars = 10;
+    // $scope.oneStar = 5;
+    // $scope.twoStars = 10;
+    // $scope.threeStars = 5;
+    // $scope.fourStars = 10;
+    // $scope.fiveStars = 5;
+    //  starCounter();
+
+
+    //  CHART RECIPES BY STAR RATING: 
+    function buildGraphs() {
+
+        $scope.starOptions = {
             chart: {
                 type: 'pieChart',
                 height: 350,
                 donut: true,
-                x: function(d){return d.key;},
-                y: function(d){return d.y;},
+                x: function(d) { return d.key; },
+                y: function(d) { return d.y; },
                 showLabels: true,
 
                 pie: {
-                    startAngle: function(d) { return d.startAngle/2 -Math.PI/2 },
-                    endAngle: function(d) { return d.endAngle/2 -Math.PI/2 }
+                    startAngle: function(d) { return d.startAngle / 2 - Math.PI / 2 },
+                    endAngle: function(d) { return d.endAngle / 2 - Math.PI / 2 }
                 },
                 duration: 500,
                 legend: {
@@ -92,72 +80,77 @@ buildZeros();
         $scope.starData = [
             {
                 key: "One Star",
-                y: 5
+                y: $scope.starRatings[1] ? $scope.starRatings[1].dishes.length : 0
             },
             {
                 key: "Two Stars",
-                y: 2
+                y: $scope.starRatings[2] ? $scope.starRatings[2].dishes.length : 0
             },
             {
                 key: "Three Stars",
-                y: 9
+                y: $scope.starRatings[3] ? $scope.starRatings[3].dishes.length : 0
             },
             {
                 key: "Four Stars",
-                y: 7
+                y: $scope.starRatings[4] ? $scope.starRatings[4].dishes.length : 0
             },
             {
                 key: "Five Stars",
-                y: 4
+                y: $scope.starRatings[5] ? $scope.starRatings[5].dishes.length : 0
             },
+            {
+                key: "Unrated Dishes",
+                y: $scope.starRatings['unrated'] ? $scope.starRatings['unrated'].dishes.length : 0
+            }
         ];
 
+    }
 
-//  CHART RECIPES BY DISH TYPE:
- 
- $scope.dishTypeOptions = {
-            chart: {
-                type: 'pieChart',
-                height: 350,
-                donut: true,
-                x: function(d){return d.key;},
-                y: function(d){return d.y;},
-                showLabels: true,
+    //  CHART RECIPES BY DISH TYPE:
 
-                pie: {
-                    startAngle: function(d) { return d.startAngle/2 -Math.PI/2 },
-                    endAngle: function(d) { return d.endAngle/2 -Math.PI/2 }
-                },
-                duration: 500,
-                legend: {
-                    margin: {
-                        top: 5,
-                        right: 70,
-                        bottom: -20,
-                        left: 0
-                    }
+    $scope.dishTypeOptions = {
+        chart: {
+            type: 'pieChart',
+            height: 350,
+            donut: true,
+            x: function(d) { return d.key; },
+            y: function(d) { return d.y; },
+            showLabels: true,
+
+            pie: {
+                startAngle: function(d) { return d.startAngle / 2 - Math.PI / 2 },
+                endAngle: function(d) { return d.endAngle / 2 - Math.PI / 2 }
+            },
+            duration: 500,
+            legend: {
+                margin: {
+                    top: 5,
+                    right: 70,
+                    bottom: -20,
+                    left: 0
                 }
             }
-        };
+        }
+    };
 
-        $scope.dishTypeData = [
-            {
-                key: "Main Entre",
-                y: 5
-            },
-            {
-                key: "Side Dish",
-                y: 3
-            },
-            {
-                key: "Salads",
-                y: 7
-            },
-            {
-                key: "Desserts",
-                y: 4
-            },
-            
-        ];
+    $scope.dishTypeData = [
+        {
+            key: "Main Entre",
+            y: 5
+        },
+        {
+            key: "Side Dish",
+            y: 3
+        },
+        {
+            key: "Salads",
+            y: 7
+        },
+        {
+            key: "Desserts",
+            y: 4
+        },
+
+    ];
 
 })
