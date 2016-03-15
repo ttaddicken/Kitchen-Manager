e6e592502b88877f50a0e73301b4a4726783f36e
ttaddicken <tjtaddicken@hotmail.com>
2016-03-15 17:17:40 -0600

    new ingredient and qty working on create dish

--- public/app/views/dishes/dishCreation-controller.js
+++ public/app/views/dishes/dishCreation-controller.js
@@ -1,11 +1,13 @@
-app.controller('DishCreationController', function ($scope, $rootScope, DishService, $firebaseArray) {
+app.controller('DishCreationController', function($scope, $rootScope, DishService, $firebaseArray) {
+       
 
     // links recipies/dishes from firebase
-     $scope.dishes = DishService.getDishes();
+    $scope.dishes = DishService.getDishes();
 
-    $scope.createDish = function (newDish) {
+    $scope.createDish = function(newDish) {
+        
         newDish.creationDate = Date.now();
-        $scope.dishes.$add($scope.newDish).then(function(dish){
+        $scope.dishes.$add($scope.newDish).then(function(dish) {
             $rootScope.member.dishList = $rootScope.member.dishList || {}
             $rootScope.member.dishList[dish.key()] = $scope.newDish;
             $rootScope.member.$save();
@@ -13,45 +15,20 @@ app.controller('DishCreationController', function ($scope, $rootScope, DishServi
 
         });
     }
-    
+
     $scope.clearSearch = function() {
         $scope.newIngredient = '';
-}
 
-    // clearRecipe()
-    // $scope.clearDish = function (clearRecipe) {
-    //     $rootScope.member.recipes[ref.key()] = '';
-    // }    
+    }
+  
+    $scope.addIngredient = function(newIngredient) {
+        debugger;
+         $scope.newDish.ingredients =  $scope.newDish.ingredients || [];
+        $scope.newDish.ingredients.push($scope.newIngredient);
+        $scope.clearSearch()
+    }
 
-
-    // WE NEED THESE FUNCTIONS:
-    // addToFavorites()
-    // addIngredient()
-    // importToFavorites()
-
-// });
-
-//CREATE A STAR RATING CAPABILITY TO CREATE A DISH **Also needed for Manage Dishes
-
-
-//     $scope.rate = 0;
-//     $scope.max = 5;
-//     $scope.isReadonly = false;
-
-//     $scope.hoveringOver = function (value) {
-//         $scope.overStar = value;
-//         $scope.percent = 100 * (value / $scope.max);
-//     };
-
-//   $scope.rate = 5;
-//     $scope.max = 5;
-//     $scope.isReadonly = false;
-
-//     $scope.hoveringOver = function (value) {
-//         $scope.overStar = value;
-//         $scope.percent = 100 * (value / $scope.max);
-//     };
 
     $scope.ratingStates = DishService.ratingStates;
 
-  });
+});
