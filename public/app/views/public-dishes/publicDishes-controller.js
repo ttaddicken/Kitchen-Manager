805d26439a63ae4814adcfbb79e6d933dbca6816
John Kent <jf.kent@yahoo.com>
2016-03-22 15:23:31 -0600

    more work on API controller

--- public/app/views/public-dishes/publicDishes-controller.js
+++ public/app/views/public-dishes/publicDishes-controller.js
@@ -5,3 +5,26 @@ app.controller('PublicDishesController', function ($firebaseArray, $scope, DishS
  $scope.indatabase = DishService.getDishes();
 //  console.log($scope.indatabase, $scope.indatabase.length);
 });
+
+app.controller('ApiController', function($scope, EdamamService){
+	$scope.findDishes = function(){
+		EdamamService.search($scope.query).success(function(results){
+				debugger
+			$scope.results = results.hits.map(function(hit){
+				var recipe = hit.recipe || {}
+				//fix ingredients
+				return {
+					img: recipe.image,
+					prepTime: '',
+					cookTime: '',
+					ingredients: recipe.ingredients,
+					name: recipe.label,
+					instructions: recipe.shareAs,
+					nutrientFacts: recipe.facts,
+					
+				}
+			});
+		})
+		
+	}
+});	
