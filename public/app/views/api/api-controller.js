959265df5fc4ef9d79c637ec001cad3d53711d26
John Kent <jf.kent@yahoo.com>
2016-03-28 12:10:05 -0600

    updates to API & excess test code in public dishes.html.

--- public/app/views/api/api-controller.js
+++ public/app/views/api/api-controller.js
@@ -1,23 +1,41 @@
-app.controller('ApiController', function($scope, EdamamService){
+app.controller('ApiController', function($scope,  $state, EdamamService){
 	$scope.findDishes = function(){
 		EdamamService.search($scope.query).success(function(results){
-				debugger
+			console.log(results);
 			$scope.results = results.hits.map(function(hit){
-				var recipe = hit.recipe || {}
+				var dish = hit.recipe || {}
 				//fix ingredients
 				return {
-					img: recipe.image,
+					title: dish.label,
+					img: dish.image,
 					prepTime: '',
 					cookTime: '',
-					ingredients: recipe.ingredients,
-					name: recipe.label,
-					instructions: recipe.shareAs,
-					nutrientFacts: recipe.facts,
+					ingredients: dish.ingredientLines,
+					instructionUrl: dish.shareAs,
+					nutrientFacts: dish.digest,
 					
 				}
 			});
 		})
 		
 	}
+	
+	    $scope.saveRecipe = function(dish) {
+			console.log(dish);
+        $state.go("dishCreation");
+
+    }
 });	
 	
+					/*  MY DISH CREATION SCHEMA
+					dish.img: dish.image,
+					{{newDish.title}}
+					{{newDish.prepTime}}
+					{{newDish.cookTime}}
+					
+					cookTime: '',
+					ingredients: recipe.ingredients,
+					name: recipe.label,
+					instructions: recipe.shareAs,
+					nutrientFacts: recipe.facts,
+					*/
